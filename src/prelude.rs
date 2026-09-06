5bbe5290153c4d4287bb2fdf79ba9000ce97ca2a
StarlightSora <300897738+StarlightSora@users.noreply.github.com>
2026-09-06 23:42:09 +0900

    prelude.rs now actually reexports meaningful stuff

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,16 @@
-pub use super::fluorite_cast::FluoriteCast;
+use super::fluorite_cast;
+use super::fluorite_cast_config;
+use super::fluorite_cast::builtins;
+
+pub use fluorite_cast::FluoriteCast;
+pub use fluorite_cast::FluoriteSpaceCastResult;
+
+pub use builtins::FluoriteBuiltinConfig;
+
+pub use fluorite_cast_config::CollisionDetectionMode;
+pub use fluorite_cast_config::MaybeExecuteCodeVia;
+pub use fluorite_cast_config::FluoriteCastCfgGravity;
+pub use fluorite_cast_config::FluoriteCastCfgHitDetection;
+pub use fluorite_cast_config::FluoriteCastCfgBuiltinFlags;
+pub use fluorite_cast_config::FluoriteCastCfgMethods;
+pub use fluorite_cast_config::FluoriteCastConfig;
